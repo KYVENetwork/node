995cfffa580258d7023c298eeafa3a08df34ee66
RuslanGlaznyov <ruslan.glaznyov@gmail.com>
2022-08-25 12:47:03 +0300

    feat(query): new methods

--- common/sdk/src/clients/lcd-client/query/v1beta1/query.ts
+++ common/sdk/src/clients/lcd-client/query/v1beta1/query.ts
@@ -1,6 +1,18 @@
 import { kyve, cosmos } from "@kyve/proto";
-import kyveQuery = kyve.registry.v1beta1.kyveQuery;
-import kyveQueryRes = kyve.registry.v1beta1.kyveQueryRes;
+import kyveQuery = kyve.query.v1beta1.kyveQuery;
+import kyveQueryRes = kyve.query.v1beta1.kyveQuery;
+
+import kyveQueryAccount = kyve.query.v1beta1.kyveQueryAccount;
+import kyveQueryAccountRes = kyve.query.v1beta1.kyveQueryAccountRes;
+import kyveQueryBundles = kyve.query.v1beta1.kyveQueryBundles;
+import kyveQueryBundlesRes = kyve.query.v1beta1.kyveQueryBundlesRes;
+import kyveQueryPools = kyve.query.v1beta1.kyveQueryPools;
+import kyveQueryPoolsRes = kyve.query.v1beta1.kyveQueryPoolsRes;
+import kyveQueryStakersRes = kyve.query.v1beta1.kyveQueryStakersRes;
+import kyveQueryStakers = kyve.query.v1beta1.kyveQueryStakers;
+import kyveQueryDelegation = kyve.query.v1beta1.kyveQueryDelegation;
+import kyveQueryDelegationRes = kyve.query.v1beta1.kyveQueryDelegationRes;
+
 import PageRequest = cosmos.registry.v1beta1.cosmosPaginationQuery.PageRequest;
 import { AbstractKyveLCDClient } from "../../lcd-client.abstract";
 
@@ -43,17 +55,17 @@ export class KyveRegistryLCDClient extends AbstractKyveLCDClient {
   constructor(restEndpoint: string) {
     super(restEndpoint);
   }
-
+  /** Pools **/
   async pool(
-    params: kyveQuery.QueryPoolRequest
-  ): Promise<kyveQueryRes.QueryPoolResponse> {
+    params: kyveQueryPools.QueryPoolRequest
+  ): Promise<kyveQueryPoolsRes.QueryPoolResponse> {
     const endpoint = `/kyve/query/v1beta1/pool/${params.id}`;
     return await this.request(endpoint);
   }
 
   async pools(
-    params?: PaginationAllPartialRequestUtilType<kyveQuery.QueryPoolsRequest>
-  ): Promise<PaginationResponseTypeUtil<kyveQueryRes.QueryPoolsResponse>> {
+    params?: PaginationAllPartialRequestUtilType<kyveQueryPools.QueryPoolsRequest>
+  ): Promise<PaginationResponseTypeUtil<kyveQueryPoolsRes.QueryPoolsResponse>> {
     const parameters: Record<string, any> = {};
     if (typeof params?.pagination !== "undefined") {
       parameters.pagination = params.pagination;
@@ -74,10 +86,13 @@ export class KyveRegistryLCDClient extends AbstractKyveLCDClient {
     const endpoint = `kyve/query/v1beta1/pools`;
     return await this.request(endpoint, parameters);
   }
-
+  /** end Pools **/
+  /** Stakers **/
   async stakers(
-    params: PaginationPartialRequestUtilType<kyveQuery.QueryStakersRequest>
-  ): Promise<PaginationResponseTypeUtil<kyveQueryRes.QueryStakersResponse>> {
+    params: PaginationPartialRequestUtilType<kyveQueryStakers.QueryStakersRequest>
+  ): Promise<
+    PaginationResponseTypeUtil<kyveQueryStakersRes.QueryStakersResponse>
+  > {
     const parameters: Record<string, any> = {};
 
     if (typeof params?.pagination !== "undefined") {
@@ -88,57 +103,129 @@ export class KyveRegistryLCDClient extends AbstractKyveLCDClient {
   }
 
   async staker(
-    params: kyveQuery.QueryStakerRequest
-  ): Promise<kyveQueryRes.QueryStakerResponse> {
+    params: kyveQueryStakers.QueryStakerRequest
+  ): Promise<kyveQueryStakersRes.QueryStakerResponse> {
     const endpoint = `kyve/query/v1beta1/staker/${params.address}`;
     return await this.request(endpoint);
   }
 
   async stakersByPool(
-    params: kyveQuery.QueryStakersByPoolRequest
-  ): Promise<kyveQueryRes.QueryStakersByPoolResponse> {
+    params: kyveQueryStakers.QueryStakersByPoolRequest
+  ): Promise<kyveQueryStakersRes.QueryStakersByPoolResponse> {
     const endpoint = `kyve/query/v1beta1/stakers_by_pool/${params.pool_id}`;
+    return await this.request(endpoint);
+  }
+  /** end stakers **/
+
+  // async finalizedBundles(
+  //   params: PaginationPartialRequestUtilType<kyveQueryBundles.QueryFinalizedBundlesRequest>
+  // ): Promise<
+  //   PaginationResponseTypeUtil<kyveQueryRes.QueryFinalizedBundlesResponse>
+  // > {
+  //   const parameters: Record<string, any> = {};
+  //
+  //   if (typeof params?.pagination !== "undefined") {
+  //     parameters.pagination = params.pagination;
+  //   }
+  //   const endpoint = `kyve/query/v1beta1/finalized_bundles/${params.pool_id}`;
+  //   return await this.request(endpoint, parameters);
+  // }
+  /** Bundles **/
+  async finalizedBundle(
+    params: kyveQueryBundles.QueryFinalizedBundleRequest
+  ): Promise<kyveQueryBundles.QueryFinalizedBundleResponse> {
+    const endpoint = `kyve/query/v1beta1/finalized_bundle/${params.pool_id}/${params.id}`;
     return await this.request(endpoint);
   }
 
   async finalizedBundles(
-    params: PaginationPartialRequestUtilType<kyveQuery.QueryFinalizedBundlesRequest>
-  ): Promise<
-    PaginationResponseTypeUtil<kyveQueryRes.QueryFinalizedBundlesResponse>
-  > {
+    params: PaginationPartialRequestUtilType<kyveQueryBundles.QueryFinalizedBundlesRequest>
+  ): Promise<PaginationResponseTypeUtil<kyveQueryBundlesRes.QueryFinalizedBundlesResponse>> {
     const parameters: Record<string, any> = {};
 
     if (typeof params?.pagination !== "undefined") {
       parameters.pagination = params.pagination;
     }
-    const endpoint = `kyve/query/v1beta1/finalized_bundles/${params.pool_id}`;
+    const endpoint = `/kyve/query/v1beta1/finalized_bundles/${params.pool_id}`;
     return await this.request(endpoint, parameters);
   }
 
-  async finalizedBundle(
-    params: kyveQuery.QueryFinalizedBundleRequest
-  ): Promise<kyveQueryRes.QueryFinalizedBundleResponse> {
-    const endpoint = `kyve/query/v1beta1/finalized_bundle/${params.pool_id}/${params.id}`;
+  async currentVoteStatus(
+    params: kyveQueryBundles.QueryCurrentVoteStatusRequest
+  ): Promise<kyveQueryBundlesRes.QueryCurrentVoteStatusResponse> {
+    const endpoint = `/kyve/query/v1beta1/current_vote_status/${params.pool_id}`;
     return await this.request(endpoint);
   }
 
-  /* ProposalByHeight ... */
   async canValidate(
-    params: kyveQuery.QueryCanValidateRequest
-  ): Promise<kyveQueryRes.QueryCanValidateResponse> {
+    params: kyveQueryBundles.QueryCanValidateRequest
+  ): Promise<kyveQueryBundles.QueryCanValidateResponse> {
     const endpoint = `kyve/query/v1beta1/can_validate/${params.pool_id}/${params.valaddress}`;
     return await this.request(endpoint);
   }
   async canPropose(
-    params: kyveQuery.QueryCanProposeRequest
-  ): Promise<kyveQueryRes.QueryCanProposeResponse> {
+    params: kyveQueryBundles.QueryCanProposeRequest
+  ): Promise<kyveQueryBundles.QueryCanProposeResponse> {
     const endpoint = `kyve/query/v1beta1/can_propose/${params.pool_id}/${params.staker}/${params.proposer}/${params.from_height}`;
     return await this.request(endpoint);
   }
   async canVote(
-    params: kyveQuery.QueryCanVoteRequest
-  ): Promise<kyveQueryRes.QueryCanVoteResponse> {
+    params: kyveQueryBundles.QueryCanVoteRequest
+  ): Promise<kyveQueryBundles.QueryCanVoteResponse> {
     const endpoint = `kyve/query/v1beta1/can_vote/${params.pool_id}/${params.staker}/${params.voter}/${params.storage_id}`;
     return await this.request(endpoint);
   }
+  /** end Bundles **/
+  /** Delegations **/
+  async delegator(
+    params: kyveQueryDelegation.QueryDelegatorRequest
+  ): Promise<kyveQueryDelegationRes.QueryDelegatorResponse> {
+    const endpoint = `/kyve/delegation/v1beta1/delegator/${params.staker}/${params.delegator}`;
+    return await this.request(endpoint);
+  }
+  async delegatorByStaker(
+    params: kyveQueryDelegation.QueryDelegatorsByStakerRequest
+  ): Promise<kyveQueryDelegationRes.QueryDelegatorsByStakerRequest> {
+    const endpoint = `/kyve/delegation/v1beta1/delegators_by_staker/${params.staker}`;
+    return await this.request(endpoint);
+  }
+  async stakersByDelegator(
+    params: PaginationPartialRequestUtilType<kyveQueryDelegation.QueryStakersByDelegatorRequest>
+  ): Promise<PaginationResponseTypeUtil<kyveQueryDelegationRes.QueryStakersByDelegatorResponse>> {
+    const parameters: Record<string, any> = {};
+
+    if (typeof params?.pagination !== "undefined") {
+      parameters.pagination = params.pagination;
+    }
+    const endpoint = `/kyve/delegation/v1beta1/stakers_by_delegator/${params.delegator}`;
+    return await this.request(endpoint, parameters);
+  }
+  /** end Delegations **/
+
+  /*** Account **/
+  async accountAssets(
+    params: kyveQueryAccount.QueryAccountAssetsRequest
+  ): Promise<kyveQueryAccountRes.QueryAccountAssetsResponse> {
+    const endpoint = `/kyve/registry/v1beta1/account_assets/${params.address}`;
+    return await this.request(endpoint);
+  }
+  async accountDelegationUnbondings(
+    params: PaginationPartialRequestUtilType<kyveQueryAccount.QueryAccountDelegationUnbondingsRequest>
+  ): Promise<PaginationResponseTypeUtil<kyveQueryAccountRes.QueryAccountDelegationUnbondingsResponse>> {
+    const endpoint = `/kyve/registry/v1beta1/account_assets/${params.address}`;
+    return await this.request(endpoint);
+  }
+  async accountFundedList(
+    params: kyveQueryAccount.QueryAccountFundedListRequest
+  ): Promise<kyveQueryAccountRes.QueryAccountFundedListResponse> {
+    const endpoint = `/kyve/registry/v1beta1/account_funded_list/${params.address}`;
+    return await this.request(endpoint);
+  }
+  async accountRedelegation(
+    params: kyveQueryAccount.QueryAccountRedelegationRequest
+  ): Promise<kyveQueryAccountRes.QueryAccountRedelegationResponse> {
+    const endpoint = `/kyve/registry/v1beta1/account_redelegation/${params.address}`;
+    return await this.request(endpoint);
+  }
+  /** End Account **/
 }
