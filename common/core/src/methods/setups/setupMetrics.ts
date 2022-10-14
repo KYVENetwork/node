f156e2a9b4dfa2ae0aa2fa0f5a15c1206a89ac7d
Troy Kessler <troy.kessler99@gmail.com>
2022-10-14 10:29:53 +0200

    further improved debug logs

--- common/core/src/methods/setups/setupMetrics.ts
+++ common/core/src/methods/setups/setupMetrics.ts
@@ -16,7 +16,7 @@ export function setupMetrics(this: Node): void {
     // init metric parameters
     this.m = {} as IMetrics;
 
-    this.logger.debug(`Initalizing metrics: prometheus default metrics`);
+    this.logger.debug(`Initializing metrics: prometheus default metrics`);
     prom_client.collectDefaultMetrics({
       labels: { app: "kyve-core" },
     });
@@ -24,7 +24,9 @@ export function setupMetrics(this: Node): void {
     // TX METRICS
 
     // MsgClaimUploaderRole metrics
-    this.logger.debug(`Initalizing metrics: tx_claim_uploader_role_successful`);
+    this.logger.debug(
+      `Initializing metrics: tx_claim_uploader_role_successful`
+    );
 
     this.m.tx_claim_uploader_role_successful = new prom_client.Counter({
       name: "tx_claim_uploader_role_successful",
@@ -32,7 +34,7 @@ export function setupMetrics(this: Node): void {
     });
 
     this.logger.debug(
-      `Initalizing metrics: tx_claim_uploader_role_unsuccessful`
+      `Initializing metrics: tx_claim_uploader_role_unsuccessful`
     );
 
     this.m.tx_claim_uploader_role_unsuccessful = new prom_client.Counter({
@@ -40,7 +42,7 @@ export function setupMetrics(this: Node): void {
       help: "The amount of MsgClaimUploaderRole txs with receipt code != 0.",
     });
 
-    this.logger.debug(`Initalizing metrics: tx_claim_uploader_role_failed`);
+    this.logger.debug(`Initializing metrics: tx_claim_uploader_role_failed`);
 
     this.m.tx_claim_uploader_role_failed = new prom_client.Counter({
       name: "tx_claim_uploader_role_failed",
@@ -49,7 +51,7 @@ export function setupMetrics(this: Node): void {
 
     // MsgVoteBundleProposal metrics
     this.logger.debug(
-      `Initalizing metrics: tx_vote_bundle_proposal_successful`
+      `Initializing metrics: tx_vote_bundle_proposal_successful`
     );
 
     this.m.tx_vote_bundle_proposal_successful = new prom_client.Counter({
@@ -58,7 +60,7 @@ export function setupMetrics(this: Node): void {
     });
 
     this.logger.debug(
-      `Initalizing metrics: tx_vote_bundle_proposal_unsuccessful`
+      `Initializing metrics: tx_vote_bundle_proposal_unsuccessful`
     );
 
     this.m.tx_vote_bundle_proposal_unsuccessful = new prom_client.Counter({
@@ -66,7 +68,7 @@ export function setupMetrics(this: Node): void {
       help: "The amount of MsgVoteBundleProposal txs with receipt code != 0.",
     });
 
-    this.logger.debug(`Initalizing metrics: tx_vote_bundle_proposal_failed`);
+    this.logger.debug(`Initializing metrics: tx_vote_bundle_proposal_failed`);
 
     this.m.tx_vote_bundle_proposal_failed = new prom_client.Counter({
       name: "tx_vote_bundle_proposal_failed",
@@ -75,7 +77,7 @@ export function setupMetrics(this: Node): void {
 
     // MsgSubmitBundleProposal metrics
     this.logger.debug(
-      `Initalizing metrics: tx_submit_bundle_proposal_successful`
+      `Initializing metrics: tx_submit_bundle_proposal_successful`
     );
 
     this.m.tx_submit_bundle_proposal_successful = new prom_client.Counter({
@@ -84,7 +86,7 @@ export function setupMetrics(this: Node): void {
     });
 
     this.logger.debug(
-      `Initalizing metrics: tx_submit_bundle_proposal_unsuccessful`
+      `Initializing metrics: tx_submit_bundle_proposal_unsuccessful`
     );
 
     this.m.tx_submit_bundle_proposal_unsuccessful = new prom_client.Counter({
@@ -92,7 +94,7 @@ export function setupMetrics(this: Node): void {
       help: "The amount of MsgSubmitBundleProposal txs with receipt code != 0.",
     });
 
-    this.logger.debug(`Initalizing metrics: tx_submit_bundle_proposal_failed`);
+    this.logger.debug(`Initializing metrics: tx_submit_bundle_proposal_failed`);
 
     this.m.tx_submit_bundle_proposal_failed = new prom_client.Counter({
       name: "tx_submit_bundle_proposal_failed",
@@ -100,7 +102,7 @@ export function setupMetrics(this: Node): void {
     });
 
     // MsgSkipUploaderRole metrics
-    this.logger.debug(`Initalizing metrics: tx_skip_uploader_role_successful`);
+    this.logger.debug(`Initializing metrics: tx_skip_uploader_role_successful`);
 
     this.m.tx_skip_uploader_role_successful = new prom_client.Counter({
       name: "tx_skip_uploader_role_successful",
@@ -108,7 +110,7 @@ export function setupMetrics(this: Node): void {
     });
 
     this.logger.debug(
-      `Initalizing metrics: tx_skip_uploader_role_unsuccessful`
+      `Initializing metrics: tx_skip_uploader_role_unsuccessful`
     );
 
     this.m.tx_skip_uploader_role_unsuccessful = new prom_client.Counter({
@@ -116,7 +118,7 @@ export function setupMetrics(this: Node): void {
       help: "The amount of MsgSkipUploaderRole txs with receipt code != 0.",
     });
 
-    this.logger.debug(`Initalizing metrics: tx_skip_uploader_role_failed`);
+    this.logger.debug(`Initializing metrics: tx_skip_uploader_role_failed`);
 
     this.m.tx_skip_uploader_role_failed = new prom_client.Counter({
       name: "tx_skip_uploader_role_failed",
@@ -126,14 +128,14 @@ export function setupMetrics(this: Node): void {
     // QUERY METRICS
 
     // QueryPool metrics
-    this.logger.debug(`Initalizing metrics: query_pool_successful`);
+    this.logger.debug(`Initializing metrics: query_pool_successful`);
 
     this.m.query_pool_successful = new prom_client.Counter({
       name: "query_pool_successful",
       help: "The amount of QueryPool /kyve/query/v1beta1/pool/{id} calls that succeeded.",
     });
 
-    this.logger.debug(`Initalizing metrics: query_pool_failed`);
+    this.logger.debug(`Initializing metrics: query_pool_failed`);
 
     this.m.query_pool_failed = new prom_client.Counter({
       name: "query_pool_failed",
@@ -141,21 +143,21 @@ export function setupMetrics(this: Node): void {
     });
 
     // QueryCanValidate metrics
-    this.logger.debug(`Initalizing metrics: query_can_validate_successful`);
+    this.logger.debug(`Initializing metrics: query_can_validate_successful`);
 
     this.m.query_can_validate_successful = new prom_client.Counter({
       name: "query_can_validate_successful",
       help: "The amount of QueryCanValidate /kyve/query/v1beta1/can_validate/{pool_id}/{valaddress} calls that succeeded.",
     });
 
-    this.logger.debug(`Initalizing metrics: query_can_validate_failed`);
+    this.logger.debug(`Initializing metrics: query_can_validate_failed`);
 
     this.m.query_can_validate_failed = new prom_client.Counter({
       name: "query_can_validate_failed",
       help: "The amount of QueryCanValidate /kyve/query/v1beta1/can_validate/{pool_id}/{valaddress} calls that failed.",
     });
 
-    this.logger.debug(`Initalizing metrics: query_can_propose_successful`);
+    this.logger.debug(`Initializing metrics: query_can_propose_successful`);
 
     // QueryCanPropose metrics
     this.m.query_can_propose_successful = new prom_client.Counter({
@@ -163,7 +165,7 @@ export function setupMetrics(this: Node): void {
       help: "The amount of QueryCanPropose /kyve/query/v1beta1/can_propose/{pool_id}/{staker}/{proposer}/{from_index} calls that succeeded.",
     });
 
-    this.logger.debug(`Initalizing metrics: query_can_propose_failed`);
+    this.logger.debug(`Initializing metrics: query_can_propose_failed`);
 
     this.m.query_can_propose_failed = new prom_client.Counter({
       name: "query_can_propose_failed",
@@ -171,14 +173,14 @@ export function setupMetrics(this: Node): void {
     });
 
     // QueryCanVote metrics
-    this.logger.debug(`Initalizing metrics: query_can_vote_successful`);
+    this.logger.debug(`Initializing metrics: query_can_vote_successful`);
 
     this.m.query_can_vote_successful = new prom_client.Counter({
       name: "query_can_vote_successful",
       help: "The amount of QueryCanVote /kyve/query/v1beta1/can_vote/{pool_id}/{staker}/{voter}/{storage_id} calls that succeeded.",
     });
 
-    this.logger.debug(`Initalizing metrics: query_can_vote_failed`);
+    this.logger.debug(`Initializing metrics: query_can_vote_failed`);
 
     this.m.query_can_vote_failed = new prom_client.Counter({
       name: "query_can_vote_failed",
@@ -189,7 +191,7 @@ export function setupMetrics(this: Node): void {
 
     // retrieve bundle
     this.logger.debug(
-      `Initalizing metrics: storage_provider_retrieve_successful`
+      `Initializing metrics: storage_provider_retrieve_successful`
     );
 
     this.m.storage_provider_retrieve_successful = new prom_client.Counter({
@@ -197,7 +199,7 @@ export function setupMetrics(this: Node): void {
       help: "The amount of calls to the storage provider to retrieve a bundle that succeeded.",
     });
 
-    this.logger.debug(`Initalizing metrics: storage_provider_retrieve_failed`);
+    this.logger.debug(`Initializing metrics: storage_provider_retrieve_failed`);
 
     this.m.storage_provider_retrieve_failed = new prom_client.Counter({
       name: "storage_provider_retrieve_failed",
@@ -205,14 +207,14 @@ export function setupMetrics(this: Node): void {
     });
 
     // save bundle
-    this.logger.debug(`Initalizing metrics: storage_provider_save_successful`);
+    this.logger.debug(`Initializing metrics: storage_provider_save_successful`);
 
     this.m.storage_provider_save_successful = new prom_client.Counter({
       name: "storage_provider_save_successful",
       help: "The amount of calls to the storage provider to save a bundle that succeeded.",
     });
 
-    this.logger.debug(`Initalizing metrics: storage_provider_save_failed`);
+    this.logger.debug(`Initializing metrics: storage_provider_save_failed`);
 
     this.m.storage_provider_save_failed = new prom_client.Counter({
       name: "storage_provider_save_failed",
@@ -222,21 +224,21 @@ export function setupMetrics(this: Node): void {
     // BUNDLE METRICS
 
     // bundle votes
-    this.logger.debug(`Initalizing metrics: bundles_voted_valid`);
+    this.logger.debug(`Initializing metrics: bundles_voted_valid`);
 
     this.m.bundles_voted_valid = new prom_client.Counter({
       name: "bundles_voted_valid",
       help: "The amount the validator voted valid.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_voted_invalid`);
+    this.logger.debug(`Initializing metrics: bundles_voted_invalid`);
 
     this.m.bundles_voted_invalid = new prom_client.Counter({
       name: "bundles_voted_invalid",
       help: "The amount the validator voted invalid.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_voted_abstain`);
+    this.logger.debug(`Initializing metrics: bundles_voted_abstain`);
 
     this.m.bundles_voted_abstain = new prom_client.Counter({
       name: "bundles_voted_abstain",
@@ -244,14 +246,14 @@ export function setupMetrics(this: Node): void {
     });
 
     // bundle proposals
-    this.logger.debug(`Initalizing metrics: bundles_proposed`);
+    this.logger.debug(`Initializing metrics: bundles_proposed`);
 
     this.m.bundles_proposed = new prom_client.Counter({
       name: "bundles_proposed",
       help: "The amount of bundles the validator proposed.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_round_time`);
+    this.logger.debug(`Initializing metrics: bundles_round_time`);
 
     this.m.bundles_round_time = new prom_client.Gauge({
       name: "bundles_round_time",
@@ -259,7 +261,7 @@ export function setupMetrics(this: Node): void {
     });
 
     this.logger.debug(
-      `Initalizing metrics: bundles_remaining_upload_interval_time`
+      `Initializing metrics: bundles_remaining_upload_interval_time`
     );
 
     this.m.bundles_remaining_upload_interval_time = new prom_client.Gauge({
@@ -267,28 +269,28 @@ export function setupMetrics(this: Node): void {
       help: "The time for the remaining upload interval.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_wait_for_next_round_time`);
+    this.logger.debug(`Initializing metrics: bundles_wait_for_next_round_time`);
 
     this.m.bundles_wait_for_next_round_time = new prom_client.Gauge({
       name: "bundles_wait_for_next_round_time",
       help: "The time to wait for the next proposal round.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_amount`);
+    this.logger.debug(`Initializing metrics: bundles_amount`);
 
     this.m.bundles_amount = new prom_client.Gauge({
       name: "bundles_amount",
       help: "The amount of bundles the validator participated in.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_data_items`);
+    this.logger.debug(`Initializing metrics: bundles_data_items`);
 
     this.m.bundles_data_items = new prom_client.Gauge({
       name: "bundles_data_items",
       help: "The amount of data items the validator participated in.",
     });
 
-    this.logger.debug(`Initalizing metrics: bundles_byte_size`);
+    this.logger.debug(`Initializing metrics: bundles_byte_size`);
 
     this.m.bundles_byte_size = new prom_client.Gauge({
       name: "bundles_byte_size",
@@ -297,14 +299,14 @@ export function setupMetrics(this: Node): void {
 
     // RUNTIME METRICS
 
-    this.logger.debug(`Initalizing metrics: runtime_get_data_item_successful`);
+    this.logger.debug(`Initializing metrics: runtime_get_data_item_successful`);
 
     this.m.runtime_get_data_item_successful = new prom_client.Counter({
       name: "runtime_get_data_item_successful",
       help: "The amount of successful returned data items from the runtime.",
     });
 
-    this.logger.debug(`Initalizing metrics: runtime_get_data_item_failed`);
+    this.logger.debug(`Initializing metrics: runtime_get_data_item_failed`);
 
     this.m.runtime_get_data_item_failed = new prom_client.Counter({
       name: "runtime_get_data_item_failed",
@@ -313,21 +315,21 @@ export function setupMetrics(this: Node): void {
 
     // BALANCE METRICS
 
-    this.logger.debug(`Initalizing metrics: balance_staker`);
+    this.logger.debug(`Initializing metrics: balance_staker`);
 
     this.m.balance_staker = new prom_client.Gauge({
       name: "balance_staker",
       help: "The current $KYVE balance of the staker.",
     });
 
-    this.logger.debug(`Initalizing metrics: balance_valaccount`);
+    this.logger.debug(`Initializing metrics: balance_valaccount`);
 
     this.m.balance_valaccount = new prom_client.Gauge({
       name: "balance_valaccount",
       help: "The current $KYVE balance of the valaccount.",
     });
 
-    this.logger.debug(`Initalizing metrics: balance_storage_provider`);
+    this.logger.debug(`Initializing metrics: balance_storage_provider`);
 
     this.m.balance_storage_provider = new prom_client.Gauge({
       name: "balance_storage_provider",
@@ -336,21 +338,21 @@ export function setupMetrics(this: Node): void {
 
     // CACHE METRICS
 
-    this.logger.debug(`Initalizing metrics: cache_current_items`);
+    this.logger.debug(`Initializing metrics: cache_current_items`);
 
     this.m.cache_current_items = new prom_client.Gauge({
       name: "cache_current_items",
       help: "The amount of data items currently in the cache.",
     });
 
-    this.logger.debug(`Initalizing metrics: cache_index_tail`);
+    this.logger.debug(`Initializing metrics: cache_index_tail`);
 
     this.m.cache_index_tail = new prom_client.Gauge({
       name: "cache_index_tail",
       help: "The current index of the last data item in the cache.",
     });
 
-    this.logger.debug(`Initalizing metrics: cache_index_head`);
+    this.logger.debug(`Initializing metrics: cache_index_head`);
 
     this.m.cache_index_head = new prom_client.Gauge({
       name: "cache_index_head",
